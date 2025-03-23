2e70516afaa0a0df9ed932c30ac9a2bea2b502a9
srinivasan-ad <forprogramingpurpose@gmail.com>
2025-03-24 02:26:26 +0530

    wow

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.SECRET))
 app.use("/qrcodes", express.static(path.join(__dirname, "Microservice/public/qrcodes")));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use(
   cors({
